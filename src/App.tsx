36097ba27e0db361d62ff1737219ca03df064236
Mark <yaazzik@gmail.com>
2022-05-17 16:47:18 +0300

    ABle to move fugures, added timer, and score

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import BoardComponent from "./components/BoardComponent";
+import "./App.css"
+import {Board} from "./models/Board";
+import {Player} from "./models/Player";
+import {Colors} from "./models/Colors";
+import LostFigures from "./components/LostFigures";
+import Timer from "./components/Timer";
 
-function App() {
+
+const App = () => {
+
+  const [board, setBoard] = useState(new Board());
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE));
+  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK));
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null)
+
+
+  useEffect(() => {
+    restart()
+    setCurrentPlayer(whitePlayer)
+   }, [])
+
+
+  function restart() {
+    const newBoard = new Board();
+    newBoard.initCells()
+    newBoard.addFigures()
+    setBoard(newBoard)
+  }
+
+  function changePlayer() {
+    setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Timer currentPlayer={currentPlayer} restart={restart}/>
+      <BoardComponent
+      board={board}
+      setBoard={setBoard}
+      currentPlayer={currentPlayer}
+      changePlayer={changePlayer}
+      />
+      <div>
+        <LostFigures title={"Черные фигуры"} figures={board.lostBlackFigures}/>
+      </div>
+      <div>
+        <LostFigures title={"Белые фигуры"} figures={board.lostWhiteFigures}/>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
